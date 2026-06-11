69b18a53a27c0acc76c5e748fdb21922ea2b4d9d
Ezika trust chidi <trustezika831@gmail.com>
2026-06-11 13:45:41 +0100

    "Exercise 1: implement calculateBmi function"
    Co-Authored-By: Ezika Trust Chidi <trustezika831@gmail.com>

---
+++ fs-typescript-main/healthapp/bmiCalculator.js
@@ -0,0 +1,17 @@
+export const calculateBmi = (height, weight) => {
+    const heightInMeters = height / 100;
+    const bmi = weight / (heightInMeters * heightInMeters);
+    if (bmi < 18.5) {
+        return "Underweight";
+    }
+    else if (bmi >= 18.5 && bmi < 24.9) {
+        return "Normal weight";
+    }
+    else if (bmi >= 25 && bmi < 29.9) {
+        return "Overweight";
+    }
+    else {
+        return "Obesity";
+    }
+};
+console.log(calculateBmi(180, 74)); // Normal weight
